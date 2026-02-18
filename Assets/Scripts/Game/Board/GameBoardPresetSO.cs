f541c41da3f387d29af87e64f20adfec68890229
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-02-17 16:51:31 -0800

    Create preset board SO

---
+++ Assets/Scripts/Game/Board/GameBoardPresetSO.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameBoardPresetSO", menuName = "Scriptable Objects/Game Board Preset")]
+public class GameBoardPresetSO : ScriptableObject
+{
+    [System.Serializable]
+    public class RoundEntry {
+        [SerializeField] private MinigameType minigameType;
+        [SerializeField] private bool isDouble;
+        [Tooltip("Optional. Leave blank for a random scene.")]
+        [SerializeField] private string sceneName;
+        
+        public MinigameType MinigameType => minigameType;
+        public bool IsDouble => isDouble;
+        public string SceneName => sceneName;
+
+        public RoundEntry(MinigameType minigameType, bool isDouble, string sceneName = null) {
+            this.minigameType = minigameType;
+            this.isDouble = isDouble;
+            this.sceneName = sceneName;
+        }
+    }
+
+    [SerializeField] private List<RoundEntry> roundList;
+    public List<RoundEntry> RoundList => roundList;
+}
