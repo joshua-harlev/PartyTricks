28dff48e7c40953bbf9f0c8f76b56f8fe0bb7d3a
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-03-23 14:24:05 -0700

    Move boost tweaks (#66)
    * Adjust move boost to scale better
    
    * Adjust coin spawning to better align with player movement
    
    * Fix coin miss on snap
    
    * Add player trail to Vine Swinging with Move Boost
    
    * Add cube files and particles file

--- Assets/Scripts/Minigames/Swinging/VineSwingingPlayerView.cs
+++ Assets/Scripts/Minigames/Swinging/VineSwingingPlayerView.cs
@@ -6,23 +6,50 @@ using VineSwinging.Core;
 namespace Minigames.Swinging {
     public class VineSwingingPlayerView : MonoBehaviour {
         [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private ParticleSystem boostTrailParticles;
+        private ParticleSystem.EmissionModule trailEmission;
         private PlayerContext currentPlayerContext;
         [SerializeField] private EventReference grabEvent;
         [SerializeField] private EventReference launchEvent;
         [SerializeField] private EventReference fallEvent;
         [SerializeField] private EventReference collectCoinEvent;
+
+        private bool isSnapping;
+        private bool needsSweep;
+        private bool showTrail;
+        private const float SnapSpeed = 80f;
+
+        public void Initialize(bool showTrail) {
+            this.showTrail = showTrail;
+            if (showTrail) {
+                trailEmission = boostTrailParticles.emission;
+                trailEmission.enabled = false;
+                boostTrailParticles.Play();
+            }
+        }
         
         public void Pull(PlayerContext playerContext) {
             currentPlayerContext = playerContext;
-            transform.localPosition = new Vector3(currentPlayerContext.PositionX, currentPlayerContext.PositionY);
-            transform.localRotation = Quaternion.Euler(0f, 0f, currentPlayerContext.SwingAngle * Mathf.Rad2Deg);
-            spriteRenderer.enabled = (currentPlayerContext.CurrentStateType != PlayerStateType.Falling);
+            Vector3 targetPosition = new Vector3(playerContext.PositionX, playerContext.PositionY);
+
+            if (currentPlayerContext.PendingEvents.Contains(PlayerEvent.GrabbedVine)) {
+                isSnapping = true;
+                needsSweep = true;
+            }
+
+            if (needsSweep && Vector3.Distance(transform.localPosition, targetPosition) > 1f) {
+                SweepCollectCoins(targetPosition);
+                needsSweep = false;
+            }
+            
             foreach (var pendingEvent in currentPlayerContext.PendingEvents) {
                 switch (pendingEvent) {
                     case PlayerEvent.GrabbedVine:
                         RuntimeManager.PlayOneShot(grabEvent, transform.position);
                         break;
                     case PlayerEvent.Fell:
+                        isSnapping = false;
+                        needsSweep = false;
                         RuntimeManager.PlayOneShot(fallEvent, transform.position);
                         break;
                     case PlayerEvent.Launched:
@@ -36,12 +63,54 @@ namespace Minigames.Swinging {
                 }
             }
             currentPlayerContext.ClearEvents();
+
+            if (isSnapping) {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, SnapSpeed * Time.deltaTime);
+                if (Vector3.Distance(transform.localPosition, targetPosition) < 0.05f) {
+                    isSnapping = false;
+                    transform.localPosition = targetPosition;
+                }
+            }
+            else {
+                transform.localPosition = targetPosition;
+            }
+            
+            transform.localRotation = Quaternion.Euler(0f, 0f, currentPlayerContext.SwingAngle * Mathf.Rad2Deg);
+            spriteRenderer.enabled = (currentPlayerContext.CurrentStateType != PlayerStateType.Falling);
+
+            if (showTrail) {
+                bool notFalling = playerContext.CurrentStateType != PlayerStateType.Falling;
+                trailEmission.enabled = notFalling;
+            }
         }
 
         public void CollectCoin(int value) {
             if (currentPlayerContext == null) return;
             currentPlayerContext.TotalCoinValue += value;
             currentPlayerContext.PendingEvents.Add(PlayerEvent.CollectedCoin);
+        }
+
+        public void SweepCollectCoins(Vector3 targetLocalPosition) {
+            Vector2 fromWorldPosition = (Vector2)transform.position;
+            Vector2 toWorldPosition;
+            if (transform.parent != null) {
+                toWorldPosition = transform.parent.TransformPoint(targetLocalPosition);
+            }
+            else {
+                toWorldPosition = targetLocalPosition;
+            }
+
+            Vector2 direction = toWorldPosition - fromWorldPosition;
+            float distance = direction.magnitude;
+            if (distance < 0.01f) return;
+
+            float sweepRadius = Mathf.Clamp(distance * 0.25f, 0.1f, 1.5f);
+            
+            var hits = Physics2D.CircleCastAll(fromWorldPosition, sweepRadius, direction.normalized, distance);
+            foreach (var hit in hits) {
+                var coin = hit.collider.GetComponent<SwingingCoinView>();
+                coin?.ForceCollect(this);
+            }
         }
     }
 }
