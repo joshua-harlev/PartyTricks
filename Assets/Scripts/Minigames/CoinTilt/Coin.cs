bcb955e9989386e68b8d270b7478202a5ec5f93a
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-03-28 15:43:49 -0700

    Coin Spin Direction Randomization Toggle and Refactoring (#80)
    * Add randomize spin direction toggle variable
    
    * Clean up Coin.cs

--- Assets/Scripts/Minigames/CoinTilt/Coin.cs
+++ Assets/Scripts/Minigames/CoinTilt/Coin.cs
@@ -5,23 +5,27 @@ namespace Minigames.CoinTilt {
     public class Coin : MonoBehaviour {
         [SerializeField] private CoinTypeSO coinType;
         [SerializeField] private ParticleSystem pullTrailParticles;
-        [SerializeField] private Renderer renderer;
+        [SerializeField] private Renderer coinMeshRenderer;
         [SerializeField] private Transform modelTransform;
         [SerializeField] private CoinAnimationSO coinAnimationConfig;
+        
+        public bool IsSpecialCoin => coinType.IsSpecialCoin;
+        
         private float spawnHeight;
         private int pointValue;
         private bool hasBeenCollected;
         private Transform pullTarget;
         private bool isBeingPulled;
         private float pullSpeed;
-        public bool IsSpecialCoin => coinType.IsSpecialCoin;
         private Coroutine flashCoroutine;
         private Vector3 initialModelEulerAngles;
         private float bobPhase;
         private float spinPhase;
         private float maxHeight;
         private float jitter;
-        private int spinSign;
+        private int spinSign = 1;
+
+        private const bool randomizeSpinDirection = false;
 
         public void StartPull(Transform target, float speed) {
             if (!isBeingPulled) {
@@ -33,16 +37,23 @@ namespace Minigames.CoinTilt {
         }
 
         private void Update() {
+            AnimateCoinMovement();
+            if (isBeingPulled && pullTarget != null) {
+                MoveTowardsPullTarget();
+                EnsureSpawnHeight();
+            }
+        }
+
+        private void MoveTowardsPullTarget() {
+            Vector3 direction = pullTarget.position - transform.position;
+            transform.position += direction * pullSpeed * Time.deltaTime;
+        }
+
+        private void AnimateCoinMovement() {
             float bobHeight = Mathf.Sin(Time.time * 2f + bobPhase) * coinAnimationConfig.BobMultiplier;
             modelTransform.localPosition = new Vector3(0f, bobHeight + maxHeight/2f, 0f);
             modelTransform.localRotation = Quaternion.Euler(initialModelEulerAngles.x, Time.time *
                 (coinAnimationConfig.SpinMultiplier * spinSign * jitter) + spinPhase, initialModelEulerAngles.z);
-            if (isBeingPulled && pullTarget != null) {
-                Vector3 direction = pullTarget.position - transform.position;
-                transform.position += direction * pullSpeed * Time.deltaTime;
-                
-                EnsureSpawnHeight();
-            }
         }
 
         private void EnsureSpawnHeight() {
@@ -52,20 +63,23 @@ namespace Minigames.CoinTilt {
         }
 
         private void Awake() {
+            InitializeCoinValue();
+            StartCoroutine(SpawnInAnimation());
+            RandomizePhases();
+            
+            jitter = Random.Range(0.8f, 1.2f);
+            if(randomizeSpinDirection) RandomizeSpinDirection();
+            maxHeight = coinAnimationConfig.BobMultiplier;
+            initialModelEulerAngles = modelTransform.localEulerAngles;
+        }
+
+        private void InitializeCoinValue() {
             if (coinType != null) {
                 pointValue = coinType.PointValue;
             }
             else {
                 Debug.LogError("No coin type specified");
             }
-
-            StartCoroutine(SpawnIn());
-            
-            RandomizePhases();
-            jitter = Random.Range(0.8f, 1.2f);
-            RandomizeSpinDirection();
-            maxHeight = coinAnimationConfig.BobMultiplier;
-            initialModelEulerAngles = modelTransform.localEulerAngles;
         }
 
         private void RandomizePhases() {
@@ -79,7 +93,7 @@ namespace Minigames.CoinTilt {
             } else spinSign = -1;
         }
 
-        private IEnumerator SpawnIn() {
+        private IEnumerator SpawnInAnimation() {
             Vector3 idealScale = this.modelTransform.localScale;
             modelTransform.localScale = Vector3.zero;
             float currentTime = 0f;
@@ -118,7 +132,7 @@ namespace Minigames.CoinTilt {
         }
 
         private IEnumerator FlashOut(float timeInSeconds) {
-            if (renderer == null) yield break;
+            if (coinMeshRenderer == null) yield break;
 
             float flashDuration = Mathf.Min(timeInSeconds * 0.3f, 3f);
             float timeBeforeFlashing = timeInSeconds - flashDuration;
@@ -130,7 +144,7 @@ namespace Minigames.CoinTilt {
                 float progress = elapsedTime / flashDuration;
                 float interval = Mathf.Lerp(0.25f, 0.05f, progress);
                 
-                renderer.enabled = !renderer.enabled;
+                coinMeshRenderer.enabled = !coinMeshRenderer.enabled;
                 yield return new WaitForSeconds(interval);
                 elapsedTime += interval;
             }
