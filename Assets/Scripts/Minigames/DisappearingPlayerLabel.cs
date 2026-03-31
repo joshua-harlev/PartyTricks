1288b92cc564490f3e3558f12417be986edc004b
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-03-30 19:22:38 -0700

    Add player label display toggle

--- Assets/Scripts/Minigames/DisappearingPlayerLabel.cs
+++ Assets/Scripts/Minigames/DisappearingPlayerLabel.cs
@@ -8,12 +8,21 @@ public class DisappearingPlayerLabel : MonoBehaviour {
     [Tooltip("Only enable if player rotates, for performance reasons")]
     [SerializeField] private bool LockRotation = false;
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     private float heightOffset;
     private Quaternion baseRotation;
 
     private void Awake() {
         baseRotation = transform.rotation;
         heightOffset = transform.localPosition.y;
+        UpdateLabelVisibility();
+        GameSettings.OnApplySettings += UpdateLabelVisibility;
+    }
+
+    private void UpdateLabelVisibility() {
+        if (spriteRenderer == null) return;
+        spriteRenderer.enabled = GameSettings.Misc.ShowPlayerLabels;
     }
 
     public void Start() {
@@ -27,5 +36,9 @@ public class DisappearingPlayerLabel : MonoBehaviour {
             transform.rotation = baseRotation;
             transform.position = transform.parent.position + Vector3.up * heightOffset;
         }
+    }
+
+    private void OnDestroy() {
+        GameSettings.OnApplySettings -= UpdateLabelVisibility;
     }
 }
--- Assets/Scripts/Options/MiscTabHandler.cs
+++ Assets/Scripts/Options/MiscTabHandler.cs
@@ -3,17 +3,21 @@ using UnityEngine.UIElements;
 namespace Options {
     public class MiscTabHandler : IOptionsTab {
         private Toggle randomizeCoinSpinDirectionToggle;
+        private Toggle showPlayerLabelToggle;
         
         public void Initialize(VisualElement tabRoot) {
             randomizeCoinSpinDirectionToggle = tabRoot.Q<Toggle>("Coin_Spin_Randomization_Toggle");
+            showPlayerLabelToggle = tabRoot.Q<Toggle>("Player_Label_Visibility_Toggle");
         }
 
         public void SyncToSettings() {
             randomizeCoinSpinDirectionToggle.value = GameSettings.Misc.RandomizeCoinSpinDirection;
+            showPlayerLabelToggle.value = GameSettings.Misc.ShowPlayerLabels;
         }
 
         public void RegisterCallbacks() {
             randomizeCoinSpinDirectionToggle.RegisterValueChangedCallback(evt => GameSettings.Misc.RandomizeCoinSpinDirection = evt.newValue);
+            showPlayerLabelToggle.RegisterValueChangedCallback(evt => GameSettings.Misc.ShowPlayerLabels = evt.newValue);
         }
     }
 }
