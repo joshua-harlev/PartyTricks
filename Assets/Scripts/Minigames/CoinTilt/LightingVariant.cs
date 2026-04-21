832df6d2c51381776e5923ed9577d5e9fd6fb5f9
Kamron Swingle <kamswingle@gmail.com>
2026-04-21 16:31:51 -0700

    Added sunset lighting, a 50% chance of the lighting to be either daytime, or sunset, and a debugger toggle to see what it looks like

---
+++ Assets/Scripts/Minigames/CoinTilt/LightingVariant.cs
@@ -0,0 +1,47 @@
+  using UnityEngine;
+
+  namespace Minigames.CoinTilt {
+      public class LightingVariant : MonoBehaviour {
+          [Header("References")]
+          [SerializeField] private Light directionalLight;
+
+          [Header("Daytime Settings")]
+          [SerializeField] private Color daytimeFilter = new Color(1f, 0.957f, 0.839f);
+          [SerializeField] private float daytimeTemperature = 8000f;
+          [SerializeField] private float daytimeIntensity = 2.27f;
+          [SerializeField] private Vector3 daytimeRotation = new(50f, -30f, 0f);
+
+          [Header("Sunset Settings")]
+          [SerializeField] private Color sunsetFilter = new Color(1f, 0.957f, 0.839f);
+          [SerializeField] private float sunsetTemperature = 2500f;
+          [SerializeField] private float sunsetIntensity = 1.8f;
+          [SerializeField] private Vector3 sunsetRotation = new(10f, -30f, 0f);
+
+          private bool isSunset;
+
+          private void Awake() {
+              isSunset = Random.value < 0.5f;
+              ApplyLighting(isSunset);
+          }
+
+          private void ApplyLighting(bool isSunset) {
+              directionalLight.useColorTemperature = true;
+              if (isSunset) {
+                  directionalLight.color = sunsetFilter;
+                  directionalLight.colorTemperature = sunsetTemperature;
+                  directionalLight.intensity = sunsetIntensity;
+                  directionalLight.transform.eulerAngles = sunsetRotation;
+              } else {
+                  directionalLight.color = daytimeFilter;
+                  directionalLight.colorTemperature = daytimeTemperature;
+                  directionalLight.intensity = daytimeIntensity;
+                  directionalLight.transform.eulerAngles = daytimeRotation;
+              }
+          }
+
+          public void ToggleLighting() {
+              isSunset = !isSunset;
+              ApplyLighting(isSunset);
+          }
+      }
+  }
