270a90b7f98379843cb1ae02767478a47b3ad727
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-04-23 12:16:10 -0700

    Add scene transition whoosh

--- Assets/Scripts/Game/SceneTransition.cs
+++ Assets/Scripts/Game/SceneTransition.cs
@@ -1,93 +1,94 @@
-using System;
-  using System.Collections;
-  using DG.Tweening;
-  using UnityEngine;
-  using UnityEngine.SceneManagement;
-  using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-  namespace Game {
-      public class SceneTransition : MonoBehaviour {
-          private static SceneTransition instance;
+namespace Game {
+  public class SceneTransition : MonoBehaviour {
+      private static SceneTransition instance;
 
-          [SerializeField] private float transitionDuration = 0.4f;
+      [SerializeField] private float transitionDuration = 0.4f;
+      [SerializeField] private EventReference transitionSound;
 
-          private RectTransform panel;
-          private Canvas canvas;
-          private bool isTransitioning;
+      private RectTransform panel;
+      private Canvas canvas;
+      private bool isTransitioning;
 
-          private void Awake() {
-              if (instance != null && instance != this) {
-                  Destroy(gameObject);
-                  return;
-              }
-
-              instance = this;
-              DontDestroyOnLoad(gameObject);
+      private void Awake() {
+          if (instance != null && instance != this) {
+              Destroy(gameObject);
+              return;
           }
 
-          private void Start()
-          {
-              CreateTransitionUI();
-          }
+          instance = this;
+          DontDestroyOnLoad(gameObject);
+      }
 
-          private void CreateTransitionUI() {
-              canvas = gameObject.AddComponent<Canvas>();
-              canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-              canvas.sortingOrder = 999;
+      private void Start()
+      {
+          CreateTransitionUI();
+      }
 
-              var canvasScaler = gameObject.AddComponent<CanvasScaler>();
-              canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-              canvasScaler.referenceResolution = new Vector2(1920, 1080);
+      private void CreateTransitionUI() {
+          canvas = gameObject.AddComponent<Canvas>();
+          canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+          canvas.sortingOrder = 999;
 
-              var panelObject = new GameObject("TransitionPanel");
-              panelObject.transform.SetParent(canvas.transform, false);
+          var canvasScaler = gameObject.AddComponent<CanvasScaler>();
+          canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+          canvasScaler.referenceResolution = new Vector2(1920, 1080);
 
-              var image = panelObject.AddComponent<Image>();
-              image.color = Color.black;
+          var panelObject = new GameObject("TransitionPanel");
+          panelObject.transform.SetParent(canvas.transform, false);
 
-              panel = panelObject.GetComponent<RectTransform>();
-              ResizePanel();
-          }
-          private void ResizePanel()
-          {
+          var image = panelObject.AddComponent<Image>();
+          image.color = Color.black;
 
-              panel.anchorMin = Vector2.zero;
-              panel.anchorMax = Vector2.one;
-              panel.offsetMin = Vector2.zero;
-              panel.offsetMax = Vector2.zero;
+          panel = panelObject.GetComponent<RectTransform>();
+          ResizePanel();
+      }
+      private void ResizePanel()
+      {
 
-              panel.anchoredPosition = new Vector2(-1920, 0);
-          }
+          panel.anchorMin = Vector2.zero;
+          panel.anchorMax = Vector2.one;
+          panel.offsetMin = Vector2.zero;
+          panel.offsetMax = Vector2.zero;
 
-          public static void LoadScene(string sceneName) {
-              if (instance == null || instance.isTransitioning) {
-                  SceneManager.LoadScene(sceneName);
-                  return;
-              }
+          panel.anchoredPosition = new Vector2(-1920, 0);
+      }
 
-              instance.StartCoroutine(instance.TransitionCoroutine(sceneName));
-          }
-
-          private IEnumerator TransitionCoroutine(string sceneName) {
-              isTransitioning = true;
-
-              panel.anchoredPosition = new Vector2(-1920, 0);
-              yield return panel.DOAnchorPosX(0, transitionDuration)
-                  .SetEase(Ease.InQuad)
-                  .SetUpdate(true)
-                  .WaitForCompletion();
-
+      public static void LoadScene(string sceneName) {
+          if (instance == null || instance.isTransitioning) {
               SceneManager.LoadScene(sceneName);
-
-              yield return null;
-
-              yield return panel.DOAnchorPosX(1920, transitionDuration)
-                  .SetEase(Ease.OutQuad)
-                  .SetUpdate(true)
-                  .WaitForCompletion();
-
-              panel.anchoredPosition = new Vector2(-1920, 0);
-              isTransitioning = false;
+              return;
           }
+
+          instance.StartCoroutine(instance.TransitionCoroutine(sceneName));
+      }
+
+      private IEnumerator TransitionCoroutine(string sceneName) {
+          isTransitioning = true;
+          RuntimeManager.PlayOneShot(transitionSound);
+          panel.anchoredPosition = new Vector2(-1920, 0);
+          yield return panel.DOAnchorPosX(0, transitionDuration)
+              .SetEase(Ease.InQuad)
+              .SetUpdate(true)
+              .WaitForCompletion();
+
+          SceneManager.LoadScene(sceneName);
+
+          yield return null;
+
+          yield return panel.DOAnchorPosX(1920, transitionDuration)
+              .SetEase(Ease.OutQuad)
+              .SetUpdate(true)
+              .WaitForCompletion();
+
+          panel.anchoredPosition = new Vector2(-1920, 0);
+          isTransitioning = false;
       }
   }
+}
