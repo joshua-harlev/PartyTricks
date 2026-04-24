1f28c919e53db01a63020898693e882270b58b40
Kamron Swingle <kamswingle@gmail.com>
2026-04-23 19:24:23 -0700

    Added a improved title screen with new characters, and a little rotation

---
+++ Assets/Scripts/TitleScreen/IdleRotate.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+  using UnityEngine.UI;
+
+  public class IdleRotate : MonoBehaviour
+  {
+      [SerializeField] private RectTransform[] targets;
+      [SerializeField] private float angle = 15f;
+      [SerializeField] private float speed = 2f;
+
+      private void Update()
+      {
+          float z = Mathf.Sin(Time.time * speed) * angle;
+          Quaternion rotation = Quaternion.Euler(0, 0, z);
+          foreach (var target in targets)
+          {
+              target.localRotation = rotation;
+          }
+      }
+  }
