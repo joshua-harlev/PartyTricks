47199351771bc0c5bc42fe93b8e6b70f41a19a8e
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-05-28 11:22:07 -0700

    Fix Service Locator unregister method

--- Assets/Scripts/Services/SceneServiceLocator.cs
+++ Assets/Scripts/Services/SceneServiceLocator.cs
@@ -52,8 +52,9 @@ namespace Services {
             DebugLogger.Log(LogChannel.Systems, "Service registered: " + type.Name);
         }
 
-        public void UnregisterService<T>(T service) where T : class {
-            var type = typeof(T);
+        public void UnregisterService<T>(T service) where T : class => UnregisterService(typeof(T));
+
+        public void UnregisterService(Type type) {
             services.Remove(type);
             DebugLogger.Log(LogChannel.Systems, "Service unregistered: " + type.Name);
         }
