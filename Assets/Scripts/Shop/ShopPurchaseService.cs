3e76963a1cc1545530f5dc09a575efb02c4b56c6
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-05-28 15:15:22 -0700

    Remove unused method

--- Assets/Scripts/Shop/ShopPurchaseService.cs
+++ Assets/Scripts/Shop/ShopPurchaseService.cs
@@ -1,45 +1,35 @@
-using System.Collections.Generic;
 using CoreData;
 using Services;
 using UnityEngine;
 
-public class ShopPurchaseService {
-    public List<ShopSlotSelector> ResolvePurchases(List<ShopSlotSelector> shopSlotSelectors, ShopItemUI[] shopItems) {
-        List<ShopSlotSelector> failedSelectors = new();
-        foreach (var playerSelector in shopSlotSelectors) {
-            int cost = ProcessPurchase(shopItems, playerSelector);
-            if (cost < 0) {
-                failedSelectors.Add(playerSelector);
+namespace Shop {
+    public class ShopPurchaseService {
+        private static int ProcessPurchase(ShopItemUI[] shopItems, ShopSlotSelector playerSelector) {
+            int index = playerSelector.CurrentShopItemIndex;
+            IPlayerService playerService = ServiceLocatorAccessor.GetService<IPlayerService>();
+            playerSelector.Lock();
+            playerSelector.CanAct = false;
+
+            PlayerProfile profile = playerService.GetPlayerProfile(playerSelector.PlayerIndex);
+            ShopItemUI item = shopItems[index];
+        
+            int baseCost = item.GetItemCost();
+            var powerUpService = ServiceLocatorAccessor.GetService<IPowerUpService>();
+            ShopModifiers shopModifiers = powerUpService.GetShopModifiers(profile);
+            int finalCost = shopModifiers.ApplyDiscount(baseCost);
+            bool purchaseSuccess = profile.Wallet.Buy(finalCost);
+            if (purchaseSuccess) {
+                profile.Inventory.AddItem(item.GetItem().ToDefinition());
+                return finalCost;
+            }
+            else {
+                Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " could not afford " + shopItems[index].ToString());
+                return -1;
             }
         }
-        return failedSelectors;
-    }
 
-    private static int ProcessPurchase(ShopItemUI[] shopItems, ShopSlotSelector playerSelector) {
-        int index = playerSelector.CurrentShopItemIndex;
-        IPlayerService playerService = ServiceLocatorAccessor.GetService<IPlayerService>();
-        playerSelector.Lock();
-        playerSelector.CanAct = false;
-
-        PlayerProfile profile = playerService.GetPlayerProfile(playerSelector.PlayerIndex);
-        ShopItemUI item = shopItems[index];
-        
-        int baseCost = item.GetItemCost();
-        var powerUpService = ServiceLocatorAccessor.GetService<IPowerUpService>();
-        ShopModifiers shopModifiers = powerUpService.GetShopModifiers(profile);
-        int finalCost = shopModifiers.ApplyDiscount(baseCost);
-        bool purchaseSuccess = profile.Wallet.Buy(finalCost);
-        if (purchaseSuccess) {
-            profile.Inventory.AddItem(item.GetItem().ToDefinition());
-            return finalCost;
+        public int ResolveSinglePurchase(ShopSlotSelector selector, ShopItemUI[] shopItems) {
+            return ProcessPurchase(shopItems, selector);
         }
-        else {
-            Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " could not afford " + shopItems[index].ToString());
-            return -1;
-        }
-    }
-
-    public int ResolveSinglePurchase(ShopSlotSelector selector, ShopItemUI[] shopItems) {
-        return ProcessPurchase(shopItems, selector);
     }
 }
