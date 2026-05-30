0b60ddaa224b0403c883296fcc0ead191a9b0a12
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-05-30 12:05:50 -0700

    Align namespaces for CoreData

--- Assets/Scripts/CoreData/CoinTypeSO.cs
+++ Assets/Scripts/CoreData/CoinTypeSO.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CoinTypeSO", menuName = "Scriptable Objects/CoinTypeSO")]
-public class CoinTypeSO : ScriptableObject {
-    public int PointValue;
-    [Tooltip("0-1 scale of least common to common")]
-    [Range(0f, 1f)]
-    public float SpawnWeight;
-    public GameObject CoinPrefab;
-    [Tooltip("Worth more than base coins?")]
-    public bool IsSpecialCoin;
+namespace CoreData {
+    [CreateAssetMenu(fileName = "CoinTypeSO", menuName = "Scriptable Objects/CoinTypeSO")]
+    public class CoinTypeSO : ScriptableObject {
+        public int PointValue;
+        [Tooltip("0-1 scale of least common to common")]
+        [Range(0f, 1f)]
+        public float SpawnWeight;
+        public GameObject CoinPrefab;
+        [Tooltip("Worth more than base coins?")]
+        public bool IsSpecialCoin;
+    }
 }
