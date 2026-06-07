b9d3cffaf32c42fc64215d729a86208303d90114
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-06-07 16:37:08 -0700

    Fix namespaces

--- Assets/Scripts/TitleScreen/SplashScreen.cs
+++ Assets/Scripts/TitleScreen/SplashScreen.cs
@@ -1,50 +1,51 @@
 using System;
 using System.Collections;
-using TitleScreen;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class SplashScreen : MonoBehaviour, ITitleScreenPhase {
-    [SerializeField] private UIDocument SplashScreenDocument;
-    [SerializeField] private float SecondsBetweenLogos;
+namespace TitleScreen {
+    public class SplashScreen : MonoBehaviour, ITitleScreenPhase {
+        [SerializeField] private UIDocument SplashScreenDocument;
+        [SerializeField] private float SecondsBetweenLogos;
     
-    private VisualElement fmodParent;
-    private VisualElement gameLogoParent;
+        private VisualElement fmodParent;
+        private VisualElement gameLogoParent;
     
-    public event Action OnPhaseComplete;
+        public event Action OnPhaseComplete;
 
-    private void Awake() {
-        fmodParent = SplashScreenDocument.rootVisualElement.Q("FMODPanel");
-        gameLogoParent = SplashScreenDocument.rootVisualElement.Q("GameLogoPanel");
-    }
+        private void Awake() {
+            fmodParent = SplashScreenDocument.rootVisualElement.Q("FMODPanel");
+            gameLogoParent = SplashScreenDocument.rootVisualElement.Q("GameLogoPanel");
+        }
 
-    private void OnEnable() {
-        StartCoroutine(Play());
-    }
+        private void OnEnable() {
+            StartCoroutine(Play());
+        }
 
-    private IEnumerator Play() {
-        HideGameLogo();
-        ShowFMOD();
-        yield return new WaitForSeconds(SecondsBetweenLogos);
-        HideFMOD();
-        ShowGameLogo();
-        yield return new WaitForSeconds(SecondsBetweenLogos);
-        OnPhaseComplete?.Invoke();
-    }
+        private IEnumerator Play() {
+            HideGameLogo();
+            ShowFMOD();
+            yield return new WaitForSeconds(SecondsBetweenLogos);
+            HideFMOD();
+            ShowGameLogo();
+            yield return new WaitForSeconds(SecondsBetweenLogos);
+            OnPhaseComplete?.Invoke();
+        }
 
-    private void ShowFMOD() {
-        fmodParent.style.display = DisplayStyle.Flex;
-    }
+        private void ShowFMOD() {
+            fmodParent.style.display = DisplayStyle.Flex;
+        }
     
-    private void ShowGameLogo() {
-        gameLogoParent.style.display = DisplayStyle.Flex;
-    }
+        private void ShowGameLogo() {
+            gameLogoParent.style.display = DisplayStyle.Flex;
+        }
     
-    private void HideFMOD() {
-        fmodParent.style.display = DisplayStyle.None;
-    }
+        private void HideFMOD() {
+            fmodParent.style.display = DisplayStyle.None;
+        }
     
-    private void HideGameLogo() {
-        gameLogoParent.style.display = DisplayStyle.None;
+        private void HideGameLogo() {
+            gameLogoParent.style.display = DisplayStyle.None;
+        }
     }
 }
--- Assets/Scripts/TitleScreen/TitleScreenOrchestrator.cs
+++ Assets/Scripts/TitleScreen/TitleScreenOrchestrator.cs
@@ -1,46 +1,47 @@
 using System.Collections;
 using Services;
-using TitleScreen;
 using UnityEngine;
 
-public class TitleScreenOrchestrator : MonoBehaviour
-{
-    [SerializeField] private SplashScreen SplashScreen;
-    [SerializeField] private IntroDisplay IntroDisplay;
-    [SerializeField] private MainMenu MainMenu;
-    [SerializeField] private bool ShowIntro;
-    [SerializeField] private bool ShowSplashScreen;
+namespace TitleScreen {
+    public class TitleScreenOrchestrator : MonoBehaviour
+    {
+        [SerializeField] private SplashScreen SplashScreen;
+        [SerializeField] private IntroDisplay IntroDisplay;
+        [SerializeField] private MainMenu MainMenu;
+        [SerializeField] private bool ShowIntro;
+        [SerializeField] private bool ShowSplashScreen;
 
-    private IGameFlowService gameFlowService;
+        private IGameFlowService gameFlowService;
     
-    private void Awake() {
-        gameFlowService = ServiceLocatorAccessor.GetService<IGameFlowService>();
-        SplashScreen.gameObject.SetActive(false);
-        IntroDisplay.gameObject.SetActive(false);
-        MainMenu.gameObject.SetActive(false);
-    }
-
-    private IEnumerator Start() {
-        if(gameFlowService.ShouldShowSplashScreen() && ShowSplashScreen) {
-            yield return RunPhase(SplashScreen);
-            gameFlowService.MarkSplashScreenShown();
+        private void Awake() {
+            gameFlowService = ServiceLocatorAccessor.GetService<IGameFlowService>();
+            SplashScreen.gameObject.SetActive(false);
+            IntroDisplay.gameObject.SetActive(false);
+            MainMenu.gameObject.SetActive(false);
         }
-        
-        if (ShowIntro) yield return RunPhase(IntroDisplay);
-        
-        yield return RunPhase(MainMenu);
-        gameFlowService.StartGame();
-    }
 
-    private IEnumerator RunPhase(ITitleScreenPhase phase) {
-        bool complete = false;
-        void OnComplete() => complete = true;
-        phase.OnPhaseComplete += OnComplete;
+        private IEnumerator Start() {
+            if(gameFlowService.ShouldShowSplashScreen() && ShowSplashScreen) {
+                yield return RunPhase(SplashScreen);
+                gameFlowService.MarkSplashScreenShown();
+            }
         
-        ((MonoBehaviour)phase).gameObject.SetActive(true);
-        yield return new WaitUntil(() => complete);
+            if (ShowIntro) yield return RunPhase(IntroDisplay);
         
-        phase.OnPhaseComplete -= OnComplete;
-        ((MonoBehaviour)phase).gameObject.SetActive(false);
+            yield return RunPhase(MainMenu);
+            gameFlowService.StartGame();
+        }
+
+        private IEnumerator RunPhase(ITitleScreenPhase phase) {
+            bool complete = false;
+            void OnComplete() => complete = true;
+            phase.OnPhaseComplete += OnComplete;
+        
+            ((MonoBehaviour)phase).gameObject.SetActive(true);
+            yield return new WaitUntil(() => complete);
+        
+            phase.OnPhaseComplete -= OnComplete;
+            ((MonoBehaviour)phase).gameObject.SetActive(false);
+        }
     }
 }
