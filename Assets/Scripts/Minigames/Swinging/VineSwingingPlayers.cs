7b186c18d0110a220cff45a692fba724bbf6eea7
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-08-13 11:50:23 -0700

    Refactor vine swinging players' set up logic into multiple methods

--- Assets/Scripts/Minigames/Swinging/VineSwingingPlayers.cs
+++ Assets/Scripts/Minigames/Swinging/VineSwingingPlayers.cs
@@ -24,40 +24,62 @@ namespace Minigames.Swinging {
         public float[] PlayerMagnetRadii { get; private set; }
         public float[] PlayerMagnetPullSpeed { get; private set; }
 
+        private MovementModifiers[] playerModifiers;
+        private float[][] allVinePositions;
+
         public void SetUp(IPlayerService playerService, IPowerUpService powerUpService, int playerCount, int vineCount, float vineAnchorY, int countdownDurationInSeconds) {
-            PlayerStateMachines = new PlayerStateMachine[playerCount];
-            PlayerHasMagnet = new bool[playerCount];
-            PlayerMagnetRadii = new float[playerCount];
-            PlayerMagnetPullSpeed = new float[playerCount];
-            
-            float[][] allVinePositions = new float[playerCount][];
-            MovementModifiers[] playerModifiers = new MovementModifiers[playerCount];
+            InitializeArrays(playerCount);
             int seed = Random.Range(0, 10000);
             
             for (int i = 0; i < playerCount; i++) {
                 PlayerSlot slot = playerService.PlayerSlots[i];
-                MovementModifiers movementModifiers = powerUpService.GetMovementModifiers(slot.Profile);
-                bool hasMoveBoost = movementModifiers.MoveBoostCount > 0;
-                playerModifiers[i] = movementModifiers;
-                PlayerHasMagnet[i] = (movementModifiers.MagnetCount > 0);
-                PlayerMagnetRadii[i] = playerStats.MagnetRadius * movementModifiers.MagnetCount;
-                PlayerMagnetPullSpeed[i] = playerStats.MagnetPullSpeed * movementModifiers.MagnetCount;
                 
+                MovementModifiers movementModifiers = powerUpService.GetMovementModifiers(slot.Profile);
+                playerModifiers[i] = movementModifiers;
                 
                 SwingConfig config = playerStats.CreateConfig(movementModifiers, movementModifiers.CoinSpawnRateBoostCount);
                 
-                playerViews[i].Initialize(hasMoveBoost, PlayerHasMagnet[i], config.CoinsPerGap);
-                
+                bool hasMoveBoost = movementModifiers.MoveBoostCount > 0;
+
                 var (vinePositions, phaseOffsets, periods) = trackViews[i].SpawnVines(vineCount, config.VineSpacing, vineAnchorY, config, new System.Random(seed), countdownDurationInSeconds);
                 allVinePositions[i] = vinePositions;
-                PlayerStateMachines[i] = new PlayerStateMachine(config, vinePositions, vineAnchorY, phaseOffsets, periods);
-                PlayerStateMachines[i].Start(0);
-                playerViews[i].transform.localPosition = new Vector3(
-                    PlayerStateMachines[i].PlayerContext.PositionX,
-                    PlayerStateMachines[i].PlayerContext.PositionY);
+                InitializePlayerStateMachine(vineAnchorY, i, config, vinePositions, phaseOffsets, periods);
+                
+                InitializePlayerView(i, movementModifiers, hasMoveBoost, config);
                 cameraFollows[i].Initialize(PlayerStateMachines[i].PlayerContext);
             }
             
+            SetUpCoins(playerCount, vineCount, vineAnchorY, seed);
+            InitializePlayerDisplays(playerCount, playerService);
+        }
+
+        private void InitializeArrays(int playerCount) {
+            PlayerStateMachines = new PlayerStateMachine[playerCount];
+            PlayerHasMagnet = new bool[playerCount];
+            PlayerMagnetRadii = new float[playerCount];
+            PlayerMagnetPullSpeed = new float[playerCount];
+            playerModifiers = new MovementModifiers[playerCount];
+            allVinePositions = new float[playerCount][];
+        }
+        
+        private void InitializePlayerStateMachine(float vineAnchorY, int playerIndex, SwingConfig config, float[] vinePositions,
+            float[] phaseOffsets, float[] periods) {
+            PlayerStateMachines[playerIndex] = new PlayerStateMachine(config, vinePositions, vineAnchorY, phaseOffsets, periods);
+            PlayerStateMachines[playerIndex].Start(0);
+        }
+        
+        private void InitializePlayerView(int playerIndex, MovementModifiers movementModifiers, bool hasMoveBoost,
+            SwingConfig config) {
+            PlayerHasMagnet[playerIndex] = (movementModifiers.MagnetCount > 0);
+            PlayerMagnetRadii[playerIndex] = playerStats.MagnetRadius * movementModifiers.MagnetCount;
+            PlayerMagnetPullSpeed[playerIndex] = playerStats.MagnetPullSpeed * movementModifiers.MagnetCount;
+            playerViews[playerIndex].Initialize(hasMoveBoost, PlayerHasMagnet[playerIndex], config.CoinsPerGap);
+            playerViews[playerIndex].transform.localPosition = new Vector3(
+                PlayerStateMachines[playerIndex].PlayerContext.PositionX,
+                PlayerStateMachines[playerIndex].PlayerContext.PositionY);
+        }
+        
+        private void SetUpCoins(int playerCount, int vineCount, float vineAnchorY, int seed) {
             for (int i = 0; i < playerCount; i++) {
                 var randomNumberGenerator = new System.Random(seed);
                 SwingConfig config = PlayerStateMachines[i].SwingConfig;
@@ -65,8 +87,6 @@ namespace Minigames.Swinging {
                 var trails = CoinTrailGenerator.GenerateAllTrails(vineCount, config, seed);
                 coinSpawners[i].SpawnCoinsForTrack(trails, allVinePositions[i], vineAnchorY, playerStats.CoinTypes, randomNumberGenerator, specialCoinRateMultiplier);
             }
-            
-            InitializePlayerDisplays(playerCount, playerService);
         }
         
         private void InitializePlayerDisplays(int playerCount, IPlayerService playerService) {
